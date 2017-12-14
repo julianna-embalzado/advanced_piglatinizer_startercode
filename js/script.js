15ffc68f8459adb78e8d6d1addde16273a949c49
julianna embalzado <embalzadoel08@gmail.com>
2017-12-14 01:14:29 +0000

    I remember getting the values from the inputgit add . yaygit add .

--- js/script.js
+++ js/script.js
@@ -31,7 +31,11 @@ $(document).ready(function() {
 
     }
 
-    // Create a click handler that takes is triggered when the user clicks the translate button.
+    // Create a click handler that is triggered when the user clicks the translate button.
+    $("#translate").click(function() {
+        var input = $("#word_box").val();
+        
+    });
         // Gets a word from the input box
         // Transforms the word to pig latin
         // Displays the result on the screen in the output element
